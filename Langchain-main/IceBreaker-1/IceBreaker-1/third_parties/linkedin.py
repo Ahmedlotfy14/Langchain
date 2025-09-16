7b1d62c86632b6adf1fb5bfdc13fe9b128aba694
Ahmedlotfy14 <111226151+Ahmedlotfy14@users.noreply.github.com>
2025-09-16 20:55:02 +0300

    Add files via upload

---
+++ Langchain-main/IceBreaker-1/IceBreaker-1/third_parties/linkedin.py
@@ -0,0 +1,27 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def scrape_linkedin_profile (linkedin_profile_url : str , mock : bool = False):
+    if mock :
+        linkedin_profile_url = "https://gist.githubusercontent.com/emarco177/859ec7d786b45d8e3e3f688c6c9139d8/raw/32f3c85b9513994c572613f2c8b376b633bfc43f/eden-marco-scrapin.json"
+        response = requests.get(
+            linkedin_profile_url,
+            timeout=10)
+    else :
+        api_endpoint = "https://api.scrapin.io/enrichment/profile"
+        params = {
+            "apikey" :os.environ.get("S_API_KEY"),
+            "linkedinurl" : linkedin_profile_url
+        }
+        response = requests.get(
+            api_endpoint,params,timeout=10
+        )
+
+    data = response.json().get("person")
+    return data
+
+if __name__ == "__main__":
+    print (scrape_linkedin_profile(linkedin_profile_url="............."))
